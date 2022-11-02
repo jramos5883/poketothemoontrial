cc6dd8f342058c9ed4712ed05211b224ed86c16e
jramos5883 <jramos5883@gmail.com>
2022-11-02 13:22:31 -0700

    Finished setting up my own breakpoints for website, going to use tailwind to style.  Need to decide on which pictures from grand opening to use for site.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: [],
   theme: {
+    screens: {
+      phone: "480px",
+      tablet: "768px",
+      laptop: "1024px",
+      desktop: "1200px",
+    },
+    debugScreens: {
+      position: ["top", "left"],
+    },
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("tailwindcss-debug-screens")],
+};
